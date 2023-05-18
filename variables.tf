b9cc52784dc415966076f8b350e01c13000dd93e
emilyjr <angie.strader@teranet.ca>
2023-05-18 23:30:25 +0000

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "cluster_name" {cluster_name = "azurerm_kubernetes_cluster.default.name" }
-variable "ARM_CLIENT_ID" {ARM_CLIENT_ID="0b9f0800-f971-4f22-a7be-9b33d9e9ca2e" }
-variable "ARM_CLIENT_SECRET" {ARM_CLIENT_SECRET="Dc~8Q~yv8VUb6.bMCObtn5AWEBiY6rjT0RJrYaqq" }
-variable "prefix" {prefix="astrader"}
+variable "cluster_name" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "prefix" {}
