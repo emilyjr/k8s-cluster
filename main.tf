8ead16d9d601ba95bb555e6f4ac180e153816a8b
emilyjr <angie.strader@teranet.ca>
2023-05-18 22:50:56 +0000

    initial commit

--- main.tf
+++ main.tf
@@ -7,12 +7,7 @@ module "aks" {
     version = "1.0.0"
 
     # insert required variables here
-#    cluster_name          = azurerm_kubernetes_cluster.default.name
-#    resource_group_name   = data.azurerm_resource_group.main.name
-#    ARM_CLIENT_ID         = 0b9f0800-f971-4f22-a7be-9b33d9e9ca2e
-#    ARM_CLIENT_SECRET     = Dc~8Q~yv8VUb6.bMCObtn5AWEBiY6rjT0RJrYaqq
-#    prefix                = astrader
-    azurerm_kubernetes_cluster.default.name  = var.cluster_name
+    azurerm_kubernetes_cluster.default.name = var.cluster_name
     resource_group_name   = data.azurerm_resource_group.main.name
     "0b9f0800-f971-4f22-a7be-9b33d9e9ca2e"         = var.ARM_CLIENT_ID
     "Dc~8Q~yv8VUb6.bMCObtn5AWEBiY6rjT0RJrYaqq" = var.ARM_CLIENT_SECRET
