e5366d9e6211c127ebbfa85bffc8befaaed923f4
emilyjr <angie.strader@teranet.ca>
2023-05-19 02:19:09 +0000

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,4 +1,13 @@
-variable "cluster_name" {}
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "prefix" {}
+variable "cluster_name" {
+description = "cluster name"
+}
+variable "ARM_CLIENT_ID" {
+description = "service pricince name"
+}
+variable "ARM_CLIENT_SECRET" {
+       description = "service principle secret"
+}
+variable "prefix" {
+
+description = "prefix"
+}
