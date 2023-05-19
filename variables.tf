44974f68b1636ee383f0c75bc4d2f1863da220e9
emilyjr <angie.strader@teranet.ca>
2023-05-19 14:58:41 +0000

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,17 +1,4 @@
-variable "cluster_name" {
-description = "cluster name"
-default = "azurerm_kubernetes_cluster.default.name"
-}
-variable "ARM_CLIENT_ID" {
-description = "service pricince name"
-default = "0b9f0800-f971-4f22-a7be-9b33d9e9ca2e"
-}
-variable "ARM_CLIENT_SECRET" {
-       default = "Dc~8Q~yv8VUb6.bMCObtn5AWEBiY6rjT0RJrYaqq"
-        description = "service principle secret"
-}
-variable "prefix" {
-
-description = "prefix"
-default = "astrader"
-}
+variable "cluster_name" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "prefix" {}
